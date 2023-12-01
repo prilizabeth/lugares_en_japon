06230f9a9118633ea06b7d5c5f331513a0efe510
Priscila Alvarez <p.alvgonz@gmail.com>
2023-12-01 00:46:12 -0300

    tarea de arrays lista

---
+++ data.php
@@ -0,0 +1,80 @@
+<?php
+
+function japan_data(){
+
+return [
+    [
+        'id'=>'1',
+        'nombre'=> 'Kawagoe',
+        'descripcion'=> 'Un cautivador pueblo al norte de Tokio que conserva su ambiente histórico del periodo Edo.',
+        'img' => 'https://live.staticflickr.com/65535/50009620772_5ef8cb851f_b.jpg',
+
+    ],
+    [
+        'id'=>'2',
+        'nombre'=> 'Shinagawa',
+        'descripcion'=> 'Shinagawa es una animada área junto a la bahía conocida por la isla Tennozu.',
+        'img' => 'https://live.staticflickr.com/65535/52866678692_fbe6be7d88_b.jpg',
+
+    ],
+    [
+        'id'=>'3',
+        'nombre'=> 'Kioto',
+        'descripcion'=> 'La antigua capital del país, es famosa en el mundo entero por su refinada cultura y gastronomía.',
+        'img' => 'https://live.staticflickr.com/65535/52845654003_6365203950_k.jpg',
+
+    ],
+    [
+        'id'=>'4',
+        'nombre'=> 'Namba Parks',
+        'descripcion'=> 'Es un complejo comercial y de oficinas ubicado en Namba, Osaka.',
+        'img' => 'https://live.staticflickr.com/65535/52795710554_f3fb2a5b49_k.jpg',
+
+    ],
+    [
+        'id'=>'5',
+        'nombre'=> 'Hozenji',
+        'descripcion'=> 'Templo budista íntimo junto a un callejón pintoresco, con una estatua cubierta de musgo de la deidad Fudomyoo.',
+        'img' => 'https://live.staticflickr.com/65535/52795627329_8988f267a7_k.jpg',
+
+    ],
+    [
+        'id'=>'6',
+        'nombre'=> 'Nakanoshima',
+        'descripcion'=> 'Una isla de arte contemporáneo, cultura y comercio en el centro de Osaka.',
+        'img' => 'https://live.staticflickr.com/65535/52754268682_b7153d06eb_k.jpg',
+
+    ],
+    [
+        'id'=>'7',
+        'nombre'=> 'Asakusa',
+        'descripcion'=> 'Conserva la esencia de un Tokio más antiguo, con tiendas de artesanías tradicionales y puestos de comida callejera.',
+        'img' => 'https://live.staticflickr.com/65535/52532260814_656ecfb44c_k.jpg',
+
+    ],
+    [
+        'id'=>'8',
+        'nombre'=> 'Nagoya',
+        'descripcion'=> 'El nudo ferroviario más grande del mundo conduce a castillos, excelentes museos y el planetario más grande de la Tierra.',
+        'img' => 'https://live.staticflickr.com/65535/52476046946_a5ab9d5113_k.jpg',
+
+    ],
+    [
+        'id'=>'9',
+        'nombre'=> 'Sapporo',
+        'descripcion'=> 'Una maravilla en invierno y un paraíso en verano, diversión al aire libre, excelente comida y su famosa cerveza.',
+        'img' => 'https://live.staticflickr.com/65535/52441023826_f3a3c65c2b_k.jpg',
+
+    ],
+    [
+        'id'=>'10',
+        'nombre'=> 'Akita',
+        'descripcion'=> 'Montañas, bosques, onsen energizantes y diversión al aire libre.',
+        'img' => 'https://live.staticflickr.com/65535/52428857326_8e2c6dafea_k.jpg',
+
+    ],
+];
+
+
+}
+
